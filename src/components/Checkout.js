ef15d93aba5e69dfd701ac0a13dff00eb6babbf2
Rebecca <rebeccagetsanemail@gmail.com>
2020-09-29 06:24:08 +0900

    update stripel

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -4,7 +4,7 @@ import {loadStripe} from '@stripe/stripe-js'
 import {Elements} from '@stripe/react-stripe-js'
 
 import CheckoutForm from './CheckoutForm'
-import {STRIPE_PUBLISHABLE_KEY} from '../client-config'
+const STRIPE_PUBLISHABLE_KEY = "pk_test_51HH0LFHDlZ1F32WmvquMI1hPjncu5K1rCFHXscEYj6D58ZZFqyMKEQmKlRFXe6s7hbp7dJqzeAtmg01kZKEXp0H300VWeMqO7t"
 
 const stripe = loadStripe(STRIPE_PUBLISHABLE_KEY)
 
