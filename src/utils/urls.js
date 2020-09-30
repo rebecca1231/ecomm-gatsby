c2c9f4a9f1fa2b068821bb5db1d97cafc1e730d6
Rebecca <rebeccagetsanemail@gmail.com>
2020-09-30 09:36:33 +0900

    urls with require statemnt

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,2 +1,4 @@
-
-export const API_URL = process.env.API_URL
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
+export const API_URL = process.env.API_URL || 'http://localhost:1337'
